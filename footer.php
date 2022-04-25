b19defe54a9aa365b0d3d2e79e89595619dd8ef6
Oluwadipe Godwin Jesuropo <Gwinsworld@gmail.com>
2022-04-25 10:55:57 -0700

    --- Auto-Git Commit ---

---
+++ footer.php
@@ -0,0 +1,30 @@
+<div class="foot">
+    <div class="footer-space">
+        <div style="margin: 10rem; text-align: center; color: aliceblue; font-size: larger;">Footer Space</div>
+        <div class="footer-modal">
+            <footer class="down-side">
+                <div class="share">
+                    <h2 style="color: white; margin-bottom: 5px;">Follow us on:</h2>
+                    <a href="https://twitter.com/TomiwaEsther4?t=L5hSOiijiExvaHMT9Lu_BA&s=09" class="fab fa-twitter"><span>Twitter</span></a>
+                    <a href="https://www.instagram.com/p/BjS8MWwlP2Y/" class="fab fa-instagram"><span>Instagram</span></a>
+                    <!--<a href="#" class="fab fa-linkedin"></a>-->
+                    <a href="https://wa.me/message/KQI27PBHN746M1" class="fab fa-whatsapp"><span>WhatsApp</span></a>
+                    <a href="https://www.facebook.com/tomiwa.esther1" class="fab fa-facebook"><span>Facebook</span></a>
+                    <!--<a href="#" class="fab fa-youtube"></a>-->
+                </div>
+
+                <div class="contact">
+                    <div class="fas fa-envelope"><span>tomiwaesther1@gmail.com</span></div>
+                    <div class="fas fa-phone"><span>+2348026470304, +2347041364188</span></div>
+                    <div class="fas fa-map-marker-alt"><span>Ibadan, Oyo state, Nigeria.</span></div>
+                </div>
+                <div class="contact">
+                    <a href="About.html">About Us</a>
+                </div>
+                <div class="developer">
+                    <p>Copyright &copy; 2021 <br> Powered by HBTush Comsat <br> All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
+    </div>
+</div>
