3d216135539b5630f95a8f8c84d6d656b79f0034
Oluwadipe Godwin Jesuropo <Gwinsworld@gmail.com>
2022-05-14 13:41:34 -0700

    --- Auto-Git Commit ---

--- T.js
+++ T.js
@@ -1,85 +1,112 @@
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("myBtn");
-    var job = document.getElementById("close");
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var job = document.getElementById("close");
 
-    btn.onclick = ()=> {
-        modal.style.display = "flex";
+btn.onclick = ()=> {
+    modal.style.display = "flex";
+}
+
+job.onclick = () => {
+    modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
+}
 
-    job.onclick = () => {
-       modal.style.display = "none";
-    }
+//Login interaction
 
-    window.onclick = (event) => {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+var UserAccountModal = document.getElementById("LoginModalPage")
+var Register = document.getElementById('RegistrationModalPage')
+var  UserAccountBtn = document.getElementById("user-btn")
+var UserAccountCloseBtn = document.getElementById("closeLogin")
+var RegisterClosePad = document.getElementById('closeRegistration')
 
-    //Login interaction
+function user(){
+    UserAccountModal.style.display = "flex"
+    Register.style.display = "none"
+}
 
-    var UserAccountModal = document.getElementById("LoginModalPage")
-    var Register = document.getElementById('RegistrationModalPage')
-    var  UserAccountBtn = document.getElementById("user-btn")
-    var UserAccountCloseBtn = document.getElementById("closeLogin")
-    var RegisterClosePad = document.getElementById('closeRegistration')
+function NewAccount(){
+    Register.style.display = "flex"
+    UserAccountModal.style.display = "none"
+}
 
-    function user(){
-        UserAccountModal.style.display = "flex"
+UserAccountCloseBtn.onclick = () => {
+    UserAccountModal.style.display = "none"
+}
+
+RegisterClosePad.onclick = () => {
+    Register.style.display = "none"
+}
+
+window.onclick = (event) => {
+    if (event.target == UserAccountModal) {
+        UserAccountModal.style.display = "none"
+    }else if(event.target == Register) {
         Register.style.display = "none"
     }
+}
 
-    function NewAccount(){
-        Register.style.display = "flex"
-        UserAccountModal.style.display = "none"
-    }
+//slide automation
 
-    UserAccountCloseBtn.onclick = () => {
-        UserAccountModal.style.display = "none"
-    }
+let slideIndex = 0;
 
-    RegisterClosePad.onclick = () => {
-        Register.style.display = "none"
-    }
+showSlides();
 
-    window.onclick = (event) => {
-        if (event.target == UserAccountModal) {
-            UserAccountModal.style.display = "none"
-        }else if(event.target == Register) {
-            Register.style.display = "none"
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+
+    let dots = document.getElementsByClassName("dot");
+    
+        for(i = 0; i < slides.length; i++) {
+
+            slides[i].style.display ="none";
         }
-    }
 
-    //slide automation
+    slideIndex++;
 
-    let slideIndex = 0;
+    if(slideIndex > slides.length)
+        {slideIndex = 1}
 
-    showSlides();
+        for(i = 0; i < dots.length; i++) {
 
-    function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
 
-        let dots = document.getElementsByClassName("dot");
-       
-            for(i = 0; i < slides.length; i++) {
-
-                slides[i].style.display ="none";
-            }
-
-        slideIndex++;
-
-        if(slideIndex > slides.length)
-            {slideIndex = 1}
-
-            for(i = 0; i < dots.length; i++) {
-
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
         slides[slideIndex -1].style.display = "block";
                         
         dots[slideIndex-1].className += " active";
 
         //changes image every 3 second
         setTimeout(showSlides, 3000);
+}
+
+//Menu Operation
+
+var MenuContainer = document.getElementsByClassName("nav-link-container")[0]
+// console.log(MenuContainer)
+var Menulist =document.getElementsByClassName("nav-btn")[0]
+
+var CloseTab = document.getElementsByClassName("closeX")[0]
+
+// function MenuGear() {
+   
+// }
+function MenuGear() {
+    MenuContainer.style.display = "block"
+    Menulist.style.display = "none"
+    CloseTab.style.display = "Block"
+}
+
+// MenuButton = MenuContainer.addEventListener('click', function(event) {
+//    var MenuBoard = event.target
+// })
+window.onclick = (event) => {
+    if (event.target == MenuContainer) {
+        MenuContainer.style.display = "none"
     }
+}
